b5c45e88a871aea9a5e12201de8429083d46a4c8
toobatux <toobatux@gmail.com>
2026-08-12 21:37:35 -0400

    settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
   "http://127.0.0.1:5173",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://photo-ldqrwr0zq-toobatuxs-projects.vercel.app",
 ]
 
 CORS_ALLOW_HEADERS = [
@@ -52,6 +53,7 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://photo-ldqrwr0zq-toobatuxs-projects.vercel.app",
 ]
 
 # Application definition
